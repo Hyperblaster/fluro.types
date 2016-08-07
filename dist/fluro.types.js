b461b6b3d94087f39eac5717bf7e69c7fff8ec49
Cade <cade@hyperblaster.org>
2016-08-07 17:08:44 +1000

    shifted process to be the default for the list

--- dist/fluro.types.js
+++ dist/fluro.types.js
@@ -39,7 +39,7 @@ angular.module('fluro.types')
             //     title: 'Parent',
             //     key: 'parent'
             // }],
-            viewModes: ['process' , 'list']
+            viewModes: ['process', 'default']
         });
 
         controller.types.push({
