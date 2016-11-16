0e9a99cfd33c7bd9c0b0a013b8d83cff3cd67e09
Cade <cade@hyperblaster.org>
2016-11-16 17:29:40 +1100

    Added teams for contacts

--- dist/fluro.types.js
+++ dist/fluro.types.js
@@ -331,7 +331,7 @@ angular.module('fluro.types')
                 key: 'gender'
             },
             {
-                title: 'Teams',
+                title: 'Groups / Teams',
                 key: 'teams'
             }],
             viewModes: ['duplicates'],
